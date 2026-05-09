83a31da6454c7521c090d017764a39df78f3e2d4
Liberalcap <xd48dubeyaryan@gmail.com>
2026-05-09 11:02:31 +0530

    Base URL updates

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://your-backend-name.onrender.com";
+const API_BASE_URL = "https://job-portal-backend-rvzr.onrender.com";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
